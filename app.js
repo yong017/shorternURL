565ecaa86284e28ae0f6efe28206da50127bf729
yong017 <zero0lemon@gmail.com>
2023-02-05 16:03:54 +0800

    feat: add post method

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const port = 3000
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 
+const URL = require("./models/shortren")
+const shortenURL = require("./utils/shortenURL")
+
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'produciton') {
@@ -27,6 +30,24 @@ app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  if (!req.body.url) return res.redirect("/") //name = "url"
+  const shortURL = shortenURL(5)
+
+  URL.findOne({ originalurl: req.body.url })
+    .then(data =>
+      data ? data : URL.create({ shortURL, originalURL: req.body.url })
+    )
+    .then(data =>
+      res.render("index", {
+        origin: req.headers.origin,
+        shortURL: data.shortURL,
+      })
+    )
+    .catch(error => console.log(error))
+
 })
 
 app.listen(port, () => {
--- models/shortren.js
+++ models/shortren.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const shortrenSchema = new Schema({
-  name: {
+  shortURL: {
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
   },
-  done: {
-    type: Boolean
+  originalURL: {
+    type: String,
+    required: true
   }
 })
 module.exports = mongoose.model('Shortren', shortrenSchema)
